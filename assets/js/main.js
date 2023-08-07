834e7aacc754676a78178f931b1d423670f44f1d
Josh Petras <josh@petr.as>
2023-08-07 14:16:40 -0500

    Add dropdown selector for API Endpoint.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,27 @@
   $(document).ready(function() {
     console.log('Document ready');
     // Store the API endpoint
-    var apiEndpoint = "https://i7oxndw6wa.execute-api.eu-central-1.amazonaws.com/prd/tokens"; // Production Server
+    var selectedEndpoint = document.getElementById("endpoint-selector").value;
+    var apiEndpoint = selectedEndpoint;
+    // Load the selected endpoint from localStorage (or use a default if not set)
+    var savedApiEndpoint = localStorage.getItem("selectedApiEndpoint");
+    if (savedApiEndpoint) {
+      apiEndpoint = savedApiEndpoint;
+      document.getElementById("endpoint-selector").value = savedApiEndpoint;  // Set the dropdown value
+    }
+    // var apiEndpoint = "https://i7oxndw6wa.execute-api.eu-central-1.amazonaws.com/prd/tokens"; // Production Server
     // var apiEndpoint = "https://prd.tcs31.sostark.nl/api/tokens"; // Development server
+
+    document.getElementById("endpoint-selector").addEventListener("change", function() {
+        apiEndpoint = document.getElementById("endpoint-selector").value;
+        var table = $('#sensor-data').DataTable();
+
+        // Update the DataTable's AJAX source
+        table.ajax.url(apiEndpoint).load();
+
+        // Save the selected endpoint to localStorage
+        localStorage.setItem("selectedApiEndpoint", apiEndpoint);
+    });
 
     // Get the current date and time when the query is initiated
     var dateString = moment().format('YYYY-MM-DD HH:mm:ss'); // Use moment.js to create the date string
